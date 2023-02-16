f91dcf638f871d3d5d8950a8d7f4353ec9ed6712
lyzzandro <lyzzandro91@gmail.com>
2023-02-16 12:42:46 -0300

    starting the main styles

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let blocos = {
+    bloco1: {
+        nome: "O Python do vovô não sobe mais",
+        cidade: "São Paulo - SP",
+        capa: "../assets/img_capas/img1.jpg",
+    },
+    bloco2: {
+        nome: "Todo mundo null",
+        cidade: "Florianópolis - SC",
+        capa: "../assets/img_capas/img2.jpg",
+    },
+};
