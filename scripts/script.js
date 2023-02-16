38558c08fdedd4e1c7e83d5b27b36c1d5b5b26d3
lyzzandro <lyzzandro91@gmail.com>
2023-02-16 15:11:18 -0300

    fixing images

--- scripts/script.js
+++ scripts/script.js
@@ -2,55 +2,55 @@ let blocos = [
     {
         nome: "O Python do vovô não sobe mais",
         cidade: "São Paulo - SP",
-        capa: "../assets/img_capas/img1.jpg",
+        capa: "./assets/img_capas/img1.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Todo mundo null",
         cidade: "Florianópolis - SC",
-        capa: "../assets/img_capas/img2.jpg",
+        capa: "./assets/img_capas/img2.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Hoje dou exception",
         cidade: "Curitiba - PR",
-        capa: "../assets/img_capas/img3.jpg",
+        capa: "./assets/img_capas/img3.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Manda Node",
         cidade: "Salvador - BA",
-        capa: "../assets/img_capas/img4.jpg",
+        capa: "./assets/img_capas/img4.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Só no back-end",
         cidade: "São Paulo - SP",
-        capa: "../assets/img_capas/img5.jpg",
+        capa: "./assets/img_capas/img5.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Esse anel não é de Ruby",
         cidade: "São Paulo - SP",
-        capa: "../assets/img_capas/img6.jpg",
+        capa: "./assets/img_capas/img6.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Pimenta no C# dos outros é refresco",
         cidade: "Rio de Janeiro - RJ",
-        capa: "../assets/img_capas/img7.jpg",
+        capa: "./assets/img_capas/img7.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "EnCACHE aqui",
         cidade: "Porto Alegre - RS",
-        capa: "../assets/img_capas/img8.jpg",
+        capa: "./assets/img_capas/img8.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
     {
         nome: "Não valho nada mas JAVA li",
         cidade: "São Paulo - SP",
-        capa: "../assets/img_capas/img9.jpg",
+        capa: "./assets/img_capas/img9.jpg",
         text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
     },
 ];
@@ -91,7 +91,7 @@ function addHTML() {
         div.appendChild(cidade);
 
         const icon = document.createElement("img");
-        icon.src = "../assets/location.svg";
+        icon.src = "./assets/location.svg";
         cidade.appendChild(icon);
 
         const pCidade = document.createElement("p");
